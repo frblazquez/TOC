af121481d256179664addb521ca8aab934ce9f35
Francisco Javier Blázquez Martínez <fcojavibm1@gmail.com>
2018-10-02 13:27:54 +0100

    Practise 1b updated

--- Practise 1/Practise 1b/simu_1b.vhd
+++ Practise 1/Practise 1b/simu_1b.vhd
@@ -1,4 +1,10 @@
---Librerias necesarias
+-- Francisco Javier Blázquez Martínez ~ frblazqu@ucm.es
+--
+-- Double degree in Mathematics-Computer engineering.
+-- Complutense university, Madrid.
+--
+-- Description: Test for the 4 bits register designed.
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 use ieee.std_logic_arith.all;
@@ -12,26 +18,25 @@ END simreg;
  
 ARCHITECTURE behavior OF simreg IS 
  
--- Declaraci?n del componente que vamos a simular
+-- Declaración del componente que vamos a simular
  
-    COMPONENT reg_paralelo
+    COMPONENT register4bits
     PORT(
          rst : IN  std_logic;
          clk : IN  std_logic;
-			load: IN std_logic;
-         E : IN  std_logic_vector(3 downto 0);
-         S : OUT  std_logic_vector(3 downto 0)
-        );
+			   load: IN  std_logic;
+         I   : IN  std_logic_vector(3 downto 0);
+         O   : OUT std_logic_vector(3 downto 0));
     END COMPONENT;
 
 --Entradas
     signal rst : std_logic;
     signal clk : std_logic;
-    signal E : std_logic_vector(3 downto 0);
-	 signal load: std_logic;
+    signal I   : std_logic_vector(3 downto 0);
+	  signal load: std_logic;
 		
 --Salidas
-    signal S : std_logic_vector(3 downto 0);
+    signal O : std_logic_vector(3 downto 0);
    
 --Se define el periodo de reloj 
     constant clk_period : time := 50 ns;
@@ -40,12 +45,12 @@ BEGIN
  
 -- Instanciacion de la unidad a simular 
 
-   uut: reg_paralelo PORT MAP (
+   uut: register4bits PORT MAP (
           rst => rst,
           clk => clk,
-			 load => load,
-          E => E,
-          S => S
+			    load => load,
+          I => I,
+          O => O
         );
 
 -- Definicion del process de reloj
@@ -64,32 +69,32 @@ stim_proc: process
 -- Se mantiene el rst activado durante 45 ns.
 	rst<='1';
 	load <= '0';
-	E<="0000";
+	I<="0000";
       wait for 45 ns;
 -- Dejamos de resetear	
 	rst<='0';
 	load <= '0';
-	E<="0000";
+	I<="0000";
       wait for 50 ns;	
 -- Cargamos el valor "1101" en el registro
 	rst<='0';
 	load <= '1';
-	E<="1101";
+	I<="1101";
       wait for 50 ns;	
 -- Mantenemos el valor durante 100 ns
 	rst<='0';
 	load <= '0';
-	E<="0000"; 
+	I<="0000"; 
       wait for 100 ns;	
 -- Cargamos el valor "0011" en el registro 
 	rst<='0';
 	load <= '1';
-	E<="0011"; 
+	I<="0011"; 
       wait for 50 ns;	
 -- Mantenemos el valor para siempre
 	rst<='0';
 	load <= '0';
-	E<="0000"; 
+	I<="0000"; 
       wait;	
 end process;
 
